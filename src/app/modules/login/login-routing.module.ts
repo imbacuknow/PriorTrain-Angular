47f1387b2e03123cbeffe2384d533e3f941e6849
imbac <imbac.smil3@gmail.com>
2021-07-27 19:22:22 +0700

    edit login route

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '**', redirectTo: 'login'},
   {path: "login", component: LoginComponent}
 ];
 
