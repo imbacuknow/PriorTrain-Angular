5b79923ae6da2bf62f30aee9c57fa98c15225f05
imbac <imbac.smil3@gmail.com>
2021-07-28 22:36:04 +0700

    update login-routing and logincomponent

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,10 +1,11 @@
+import { LanguageListComponent } from './../trainee/language-list/language-list.component';
 import { LoginComponent } from './login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '**', redirectTo: 'login'},
-  {path: "login", component: LoginComponent}
+  { path: "login", component: LoginComponent },
+  { path: "languageList", component: LanguageListComponent }
 ];
 
 @NgModule({
