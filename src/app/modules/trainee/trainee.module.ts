087692838c5ede36cad6eca2fa55c3684274a2d3
imbac <imbac.smil3@gmail.com>
2021-07-28 22:35:17 +0700

    generate supervisor and trainee

---
+++ src/app/modules/trainee/trainee.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LanguageListComponent } from './language-list/language-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    LanguageListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TraineeModule { }
