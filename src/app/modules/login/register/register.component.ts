a20ad252073f476f5822b4ce865039181281f3c9
imbac <imbac.smil3@gmail.com>
2021-07-29 18:01:16 +0700

    create registerpage

--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import {MatTableDataSource} from '@angular/material/table';
 import {Component, OnInit} from '@angular/core';
 
@@ -7,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  // data table
   data: any[] = [
     {
       firstname: 'aaaa',
@@ -28,9 +30,62 @@ export class RegisterComponent implements OnInit {
     },
   ];
 
+  // form group
+  form: FormGroup;
+
   mockData = new MatTableDataSource<[]>(this.data);
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setFormGroup();
+  }
+
+  setFormGroup() {
+    this.form = this.formBuilder.group({
+      firstname: [null, Validators.required],
+      lastname: [null, Validators.required],
+      email: [null, Validators.required],
+      mobile: [null, Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      console.log("hello");
+      return;
+    } else {
+      console.log(JSON.stringify(this.data));
+      this.addNewUser();
+      console.log("------------------");
+      console.log(JSON.stringify(this.data));
+    }
+  }
+
+  addNewUser() {
+    let _data = {
+      firstname: this.firstname.value,
+      lastname: this.lastname.value,
+      email: this.email.value,
+      mobile: this.mobile.value
+    }
+    this.data.push(_data);
+    this.mockData = new MatTableDataSource<[]>(this.data);
+  }
+
+  // getter
+  get firstname(): FormControl {
+    return this.form.get('firstname') as FormControl;
+  }
+  get lastname(): FormControl {
+    return this.form.get('lastname') as FormControl;
+  }
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+  get mobile(): FormControl {
+    return this.form.get('mobile') as FormControl;
+  }
 }
