a9911ad9e8e8be4eaa8817fa327b9a2eb6e9db2e
imbac <imbac.smil3@gmail.com>
2021-07-29 18:00:37 +0700

    update table

--- src/app/modules/login/component/table/table.component.ts
+++ src/app/modules/login/component/table/table.component.ts
@@ -7,16 +7,12 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  mockData: any[] = [
-    {position: 1, username: 'test1', password: '1234'},
-    {position: 2, username: 'test2', password: '1234'},
-  ];
-  @Input() dataSource = new MatTableDataSource<[]>(this.mockData);
-  displayedColumns: string[] = ['position', 'username', 'password'];
+
+  @Input() dataSource = new MatTableDataSource<[]>();
+  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'mobile'];
 
   constructor() {}
 
   ngOnInit(): void {
-    // this.dataSource = new MatTableDataSource(mockData);
   }
 }
