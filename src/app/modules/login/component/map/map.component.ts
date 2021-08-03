5469faade423505ec597ab1fe62fba86b446dd73
imbac <imbac.smil3@gmail.com>
2021-08-03 21:42:47 +0700

    set game map

--- src/app/modules/login/component/map/map.component.ts
+++ src/app/modules/login/component/map/map.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
-  
+export class MapComponent implements AfterViewInit {
+  private map: L.Map;
 
-  constructor() { }
+  private initMap(): void {
+    this.map = L.map('map', {
+      crs: L.CRS.Simple,
+      // zoom: -1,
+      maxZoom: 1,
+      minZoom: -1,
+    });
 
-  ngOnInit(): void {
+    var bounds: L.BoundsLiteral = [
+      [0, 0],
+      [1298, 1826],
+    ];
+    var image = L.imageOverlay('assets/picture/a.png', bounds).addTo(this.map);
+    this.map.fitBounds(bounds);
+
+    // this.map.dragging.disable();
   }
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
